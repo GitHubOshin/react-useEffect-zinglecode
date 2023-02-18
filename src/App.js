3c0582389a181aef801d545ad7da5f29f09f5d90
OSHIN <izkp44@gmail.com>
2023-02-19 04:03:31 +0700

    #06 useEffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  return <div className="App"></div>
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    fetch('https://reqres.in/api/users?page=2')
+      .then((res) => {
+        return res.json()
+      })
+      .then((resJson) => {
+        setUsers(resJson.data)
+      })
+  }, [])
+
+  const userElements = users.map((user) => <h3 key={user.id}>{user.email}</h3>)
+
+  return (
+    <section className="app-section">
+      <div className="app-container">
+        <h1>Love you guys!</h1>
+        {userElements}
+      </div>
+    </section>
+  )
 }
 
 export default App
